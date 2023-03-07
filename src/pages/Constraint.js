39bcbb61d063b3c15f7255f61d5f7f4523de2951
riddlephoto <pongsk137@hotmail.com>
2023-03-07 17:38:29 +0700

    Add Header and navbar for constraint

--- src/pages/Constraint.js
+++ src/pages/Constraint.js
@@ -1,16 +1,21 @@
-import { Input_Cons, Navbar_Cons, Result_Cons,Graph_Cons } from './ConstraintDiagram';
+import { Input_Cons, Navbar_Cons, Result_Cons,Graph_Cons, Header_Cons} from './ConstraintDiagram';
 import styles from '../styles/Constraint/Constraint.module.css'
 
 const Constraint = () => {
   return (
+  <>
     <div classname={styles.app}>
-      <div style={{minWidth:'100%',display:'flex', flexDirection:'column', justifyContent:'center', alignItems: 'center'}}>
-        <Navbar_Cons />
+    <div className={styles.Navbar}> 
+          <Navbar_Cons />
+    </div>
+    <div style={{minWidth:'100%',display:'flex', flexDirection:'column', justifyContent:'center', alignItems: 'center'}}>
+        <Header_Cons />
         <Input_Cons />
         <Graph_Cons />
         <Result_Cons />
       </div>
     </div>
+  </>
   )
 }
 
